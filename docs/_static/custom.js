704a2de261c17f0826a4abef9138427ac943e780
iwasakishuto <cabernet.rock@gmail.com>
2021-07-08 14:25:59 +0900

    :art: Added a popup style.

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -12,8 +12,8 @@ function isFunction(func) {
 function addOnLoad(fn){
   if (isFunction(fn)){
     var old = window.onload;
-    window.onload = isFunction(old) 
-      ? function(){old();fn();} 
+    window.onload = isFunction(old)
+      ? function(){old();fn();}
       : fn
   }
 }
@@ -38,3 +38,36 @@ function addOnLoad(fn){
     });
   });
 })(jQuery);
+
+// Add Pop-Up Window for GraphViz Images.
+function addPopUpViewer(element){
+  const images = document.querySelectorAll(element);
+
+  for(let i=0; i<images.length; i++){
+    images[i].addEventListener("click", openPopUpGraphViz)
+  }
+
+  function openPopUpGraphViz(){
+    const filter = document.createElement('div');
+    filter.setAttribute("id", "pixel-viewer");
+
+    const div_img = document.createElement('img');
+    div_img.setAttribute("id",  "pixel-viewer__img");
+    div_img.setAttribute("src", this.src);
+
+    document.body.appendChild(filter);
+    filter.appendChild(div_img);
+
+    filter.addEventListener('click', close, {once: true});
+    function close(){
+      filter.className = 'fadeout';
+      filter.addEventListener("animationend",function(){
+        filter.remove();
+      });
+    }
+  }
+}
+addOnLoad(function(){
+  // addPopUpViewer(element="div.graphviz > img.graphviz");
+  addPopUpViewer(element="img.popup-img");
+});
